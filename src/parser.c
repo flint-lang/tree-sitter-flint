931643cc27e4ec574305f1b740f4335f4968bb5c
Julius Grünberg <julius.gruenberg@leghast.de>
2026-01-05 16:52:09 +0100

    Updated version number

--- src/parser.c
+++ src/parser.c
@@ -46740,8 +46740,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_flint(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 0,
-      .patch_version = 1,
+      .minor_version = 1,
+      .patch_version = 0,
     },
   };
   return &language;
